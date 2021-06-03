842f4e29b5f5335a14b7f61a93d2efaef3dec1d2
Srivathsan Venkateswaran <74530357+srivathsanvenkateswaran@users.noreply.github.com>
2021-06-03 18:03:46 +0530

    Updating full paths

--- copytext.py
+++ copytext.py
@@ -1,6 +1,7 @@
 import pyperclip
 
-with open('{INSERT THE FULL PATH OF FINAL.TXT HERE}', 'r') as f:
+with open('final.txt', 'r') as f:
 	text = f.read()
 	pyperclip.copy(text)
 
+# CHANGE ALL THE FILE PATHS TO THEIR FULL PATHS
